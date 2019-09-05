3afc86d8c7f6f0b85c1411055e193c3073611a59
gongzhijian <545972892@qq.com>
2019-09-05 18:40:42 +0800

    最终的数据源代码

--- security-demo-first/src/main/java/com/example/demo/dao/alpha/UserMapper.java
+++ security-demo-first/src/main/java/com/example/demo/dao/alpha/UserMapper.java
@@ -3,5 +3,7 @@ package com.example.demo.dao.alpha;
 import com.example.demo.domain.alpha.User;
 import tk.mybatis.mapper.common.Mapper;
 
+
+@org.apache.ibatis.annotations.Mapper
 public interface UserMapper extends Mapper<User> {
 }
