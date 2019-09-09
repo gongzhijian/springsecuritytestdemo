8b6a4b1dac84836bdf4e467a0cd18c7eebd5a567
gongzhijian <545972892@qq.com>
2019-09-09 17:23:18 +0800

    nrsc 加入用户自定义登陆，以及适配前后端分离的登陆模式，返回json格式，如果有误，引导用户返回登陆页面，成功跳转正确的页面以及json数据

---
+++ security-demo-second/src/main/java/com/gong/securitydemosecond/SecurityDemoSecondApplication.java
@@ -0,0 +1,13 @@
+package com.gong.securitydemosecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoSecondApplication.class, args);
+	}
+
+}
